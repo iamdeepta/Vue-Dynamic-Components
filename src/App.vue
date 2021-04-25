967562b679192072c37b0c4f5b8c425daa5b6cac
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2021-04-25 22:58:05 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import Tabs from "./components/Tabs";
+
+export default {
+  name: "App",
+  components: {
+    Tabs,
+  },
+};
+</script>
+
+<style>
+</style>
