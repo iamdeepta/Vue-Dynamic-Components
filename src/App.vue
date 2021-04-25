5d6c83214870fbfa38a06750bbf19291854fad3e
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2021-04-26 00:12:18 +0600

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div>
+      <b-tabs content-class="mt-3">
+        <b-tab title="Home" @click="component = 'Home'" active></b-tab>
+        <b-tab title="Contact" @click="component = 'Contact'"></b-tab>
+        <b-tab title="About" @click="component = 'About'"></b-tab>
+      </b-tabs>
+      <!-- <div>
+        <button @click="component = 'Home'">Home</button>
+        <button @click="component = 'Contact'">Contact</button>
+        <button @click="component = 'About'">About</button>
+      </div> -->
+    </div>
+
+    <component :is="component"></component>
+  </div>
 </template>
 
 <script>
-import Tabs from "./components/Tabs";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 export default {
   name: "App",
   components: {
-    Tabs,
+    Home,
+    Contact,
+    About,
+  },
+  data() {
+    return {
+      component: "Home",
+    };
   },
 };
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Home Component</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+</style>
